8c109d11ebf889c50de7d100673b6d0f8f1b55b3
Marta Rey <martarey@Martas-MacBook-Pro.local>
2021-11-23 09:00:32 +0000

    first

---
+++ src/main/java/com/hellocode/atm/App.java
@@ -0,0 +1,38 @@
+package com.hellocode.atm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private Parent window;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+       URL fileLocation = new File(
+               "<REPLACE>/atm-machine/src/main/java/com/hellocode/atm/atm.fxml")
+                .toURI().toURL();
+       Parent root = FXMLLoader.load(fileLocation);
+       stage.setScene(new Scene(root));
+       stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.hellocode.atm {
+    requires javafx.controls;
+    requires javafx.fxml;
+    exports com.hellocode.atm;
+}
